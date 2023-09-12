81e97557b42487d7959687ba2e03ddcf236877c3
Maxnikit <max.nikit.03@gmail.com>
2023-09-12 22:20:41 +0300

    Replace html and js with correct ones

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
-console.log("Hello World!");
+function getComputerChoice() {
+  let moves = ["Rock", "Paper", "Scissors"];
+  return moves[Math.floor(Math.random() * moves.length)];
+}
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection == "Rock") {
+    if (computerSelection == "Rock") {
+      return "Draw.";
+    } else if (computerSelection == "Paper") {
+      return "You lose! Paper beats rock.";
+    } else if (computerSelection == "Scissors") {
+      return "You win! Rock beats scissors.";
+    } else return "error";
+  }
+  if (playerSelection == "Paper") {
+    if (computerSelection == "Rock") {
+      return "You win! Paper beats Rock.";
+    } else if (computerSelection == "Paper") {
+      return "Draw.";
+    } else if (computerSelection == "Scissors") {
+      return "You lose! Scissors beats paper.";
+    } else return "error";
+  }
+  if (playerSelection == "Scissors") {
+    if (computerSelection == "Rock") {
+      return "You lose! Rock beats scissors";
+    } else if (computerSelection == "Paper") {
+      return "You win! Scissors beats paper.";
+    } else if (computerSelection == "Scissors") {
+      return "Draw.";
+    } else return "error";
+  }
+}
+// playerSelection.localeCompare(moves)
