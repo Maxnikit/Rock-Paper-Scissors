750a4be60e3a2c170af8b8ccf937cbf9a2d6b831
Maxnikit <max.nikit.03@gmail.com>
2023-09-12 22:48:35 +0300

    Make playerSelection case insensitive

--- script.js
+++ script.js
@@ -3,30 +3,32 @@ function getComputerChoice() {
   return moves[Math.floor(Math.random() * moves.length)];
 }
 function playRound(playerSelection, computerSelection) {
-  if (playerSelection == "Rock") {
-    if (computerSelection == "Rock") {
+  playerSelection =
+    playerSelection[0].toUpperCase() + playerSelection.toLowerCase().slice(1);
+  if (playerSelection === "Rock") {
+    if (computerSelection === "Rock") {
       return "Draw.";
-    } else if (computerSelection == "Paper") {
+    } else if (computerSelection === "Paper") {
       return "You lose! Paper beats rock.";
-    } else if (computerSelection == "Scissors") {
+    } else if (computerSelection === "Scissors") {
       return "You win! Rock beats scissors.";
     } else return "error";
   }
-  if (playerSelection == "Paper") {
-    if (computerSelection == "Rock") {
+  if (playerSelection === "Paper") {
+    if (computerSelection === "Rock") {
       return "You win! Paper beats Rock.";
-    } else if (computerSelection == "Paper") {
+    } else if (computerSelection === "Paper") {
       return "Draw.";
-    } else if (computerSelection == "Scissors") {
+    } else if (computerSelection === "Scissors") {
       return "You lose! Scissors beats paper.";
     } else return "error";
   }
-  if (playerSelection == "Scissors") {
-    if (computerSelection == "Rock") {
+  if (playerSelection === "Scissors") {
+    if (computerSelection === "Rock") {
       return "You lose! Rock beats scissors";
-    } else if (computerSelection == "Paper") {
+    } else if (computerSelection === "Paper") {
       return "You win! Scissors beats paper.";
-    } else if (computerSelection == "Scissors") {
+    } else if (computerSelection === "Scissors") {
       return "Draw.";
     } else return "error";
   }
